cdf94b8997569116e9968668c27d8e54b4a46a91
Shivam <ahivampatel12@gmail.com>
2026-08-16 00:40:06 +0530

    mini commit

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,24 @@
 import React from "react"
+import { Route, Routes } from "react-router"
+import Home from "./pages/Home"
+import Pricing from "./pages/Pricing"
+import Projects from "./pages/Projects"
+import Preview from "./pages/Preview"
+import Community from "./pages/Community"
+
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/projects/:projectId" element={<Projects />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/preview/:projectId" element={<Preview/>} />
+        <Route path="/preview/:projectId/:versionId" element={<Preview/>} />
+        <Route path="/community" element={<Community />} />
+      </Routes>
     </div>
   )
 }
---
+++ client/src/pages/Pricing.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import type Preview from './Preview'
+
+const Pricing = () => {
+  return (
+    <div>
+        <h1>Pricing</h1>
+    </div>
+    )
+}
+
+export default Pricing
